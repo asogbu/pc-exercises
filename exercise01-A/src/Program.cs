4fe632156016050741744c9325476636d1454dc2
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-10 21:58:44 -0600

    Exercise 01-A: add C# implementation

---
+++ exercise01-A/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            Console.WriteLine(line.Trim().Split(' ').Select(int.Parse).ToArray().Sum());
+        }
+    }
+}
