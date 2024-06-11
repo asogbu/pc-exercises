be0d651cbe12c2eb0d0f9864f4b619ea067c2dd3
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-11 08:38:06 -0600

    Exercise 01-B: add C++ implementation

--- exercise01-B/src/program.cpp
+++ exercise01-B/src/program.cpp
@@ -4,7 +4,33 @@
 #include <sstream>
 #include <string>
 
+int sum_series(int n);
+
 int main(int argc, char *argv[]) {
-    // TODO
+    for (std::string line; std::getline(std::cin, line);) {
+        std::stringstream ss(line);
+        int n;
+        ss >> n;
+        if (n == 0)
+            break;
+
+        std::cout << sum_series(n) << '\n';
+    }
+
     return 0;
+}
+
+int sum_series(int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        std::string line;
+        std::getline(std::cin, line);
+        std::stringstream ss(line);
+        int j;
+        ss >> j;
+        sum += j;
+    }
+
+    return sum;
 }
