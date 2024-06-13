64d709dbf040819546df9cff718af5f65fb29261
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-12 21:30:50 -0600

    Exercise 04-B: add Python implementation

--- exercise04-B/src/program.py
+++ exercise04-B/src/program.py
@@ -6,15 +6,35 @@ import sys
 
 # Function
 
-def is_happy(n: int, seen: set[int]) -> bool:
-    # TODO
-    return False
+
+def is_happy_cached(n: int, cache: dict[int, bool]) -> bool:
+    seen = set()
+    happiness = None
+    while n not in seen:
+        if n in cache:
+            happiness = cache[n]
+            break
+
+        seen.add(n)
+        n = sum([x * x for x in [int(char) for char in str(n)]])
+
+    if happiness is None:
+        happiness = n == 1
+
+    for s in seen:
+        cache[s] = happiness
+
+    return happiness
+
 
 # Main execution
 
-def main() -> None:
-    for n in map(int, sys.stdin):
-        print('Yes' if is_happy(n, set()) else 'No')
 
-if __name__ == '__main__':
+def main() -> None:
+    cache = {}  # Memoization: keep track of numbers with known happiness
+    for n in (int(line) for line in sys.stdin):
+        print("Yes" if is_happy_cached(n, cache) else "No")
+
+
+if __name__ == "__main__":
     main()
