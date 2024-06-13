4e9ca650ca4c4ca0a7f8d9aefdd253154144b3bf
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-12 19:23:35 -0600

    Exercise 04-A: add Python implementation

--- exercise04-A/src/program.py
+++ exercise04-A/src/program.py
@@ -2,20 +2,37 @@
 
 # Exercise 04-A: Palindromic Permutations
 
-import collections
 import sys
 
 # Functions
 
+
 def is_palindromic(s: str) -> bool:
-    # TODO
-    return False
+    return len(get_letters_with_odd_counts(s)) <= 1
+
+
+def get_letters_with_odd_counts(s: str) -> set[int]:
+    odds = set()
+    for char in s:
+        flip_membership(char, odds)
+
+    return odds
+
+
+def flip_membership(element: any, the_set: set):
+    if element in the_set:
+        the_set.remove(element)
+    else:
+        the_set.add(element)
+
 
 # Main Execution
 
+
 def main() -> None:
     for word in sys.stdin:
-        print('Yes' if is_palindromic(word.rstrip()) else 'No')
+        print("Yes" if is_palindromic(word.strip()) else "No")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
