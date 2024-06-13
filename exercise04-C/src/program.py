5ca38ff91f2bdb4171c01d0af9bafc51b6102257
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-12 19:47:13 -0600

    Exercise 04-C: add Python implementation

--- exercise04-C/src/program.py
+++ exercise04-C/src/program.py
@@ -2,20 +2,35 @@
 
 # Exercise 04-C: Contiguous Array
 
-import collections
 import sys
 
 # Functions
 
+
 def find_maximum_contiguous_array(array: list[int]) -> int:
-    return 0 
-        
+    max_length = 0
+
+    levels = {0: 0}
+    curr_level = 0
+    for index, digit in enumerate(array, 1):
+        curr_level += 1 if digit == 1 else -1
+        if curr_level in levels:
+            curr_length = index - levels[curr_level]
+            max_length = curr_length if curr_length > max_length else max_length
+        else:
+            levels[curr_level] = index
+
+    return max_length
+
+
 # Main execution
+
 
 def main() -> None:
     for line in sys.stdin:
-        digits = list(map(int, line.split()))
+        digits = [int(w) for w in line.split()]
         print(find_maximum_contiguous_array(digits))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
