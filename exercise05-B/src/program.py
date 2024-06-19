1d0c1884b393c5e5dcafd40ca7aac736a47f92fb
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-19 19:35:37 -0400

    Exercise 05-B: add Python implementation

--- exercise05-B/src/program.py
+++ exercise05-B/src/program.py
@@ -8,18 +8,37 @@ import sys
 
 # Constants
 
+numbers_to_letters = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
+
 # Functions
 
+
 def phone_combinations(numbers: str, letters: str) -> Iterator[str]:
-    # TODO: Recursively generate combinations
-    pass
+    # Recursively generate combinations
+    if not numbers:
+        yield letters
+    else:
+        for candidate in numbers_to_letters[numbers[0]]:
+            yield from phone_combinations(numbers[1:], letters + candidate)
+
 
 # Main Execution
 
+
 def main() -> None:
     for numbers in sys.stdin:
-        for combination in phone_combinations(numbers.strip(), ''):
+        for combination in phone_combinations(numbers.strip(), ""):
             print(combination)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
