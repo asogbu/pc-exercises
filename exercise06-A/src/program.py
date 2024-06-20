ba57635c02c14acf1942baec839098dded1a0b6e
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-19 20:21:12 -0400

    Exercise 06-A: add Python implementation

--- exercise06-A/src/program.py
+++ exercise06-A/src/program.py
@@ -6,15 +6,26 @@ import sys
 
 # Function
 
+
 def divisible_subsets(n: int) -> int:
-    # TODO: Determine how many subsets of {0...9} are divisible by N
-    return 0 
+    # Determine how many subsets of {0...9} are divisible by N
+
+    counter = 0
+    for subset in range(1 << 10):
+        subset_sum = sum([i if subset & (1 << i) else 0 for i in range(10)])
+        if subset_sum % n == 0:
+            counter += 1
+
+    return counter
+
 
 # Main execution
+
 
 def main() -> None:
     for n in map(int, sys.stdin):
         print(divisible_subsets(n))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
