c3e8f1d676bf6a2a5bf0000ac6d58d760358b5e8
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-06-24 23:51:36 -0400

    Exercise 08-A: add Python implementation

--- exercise08-A/src/program.py
+++ exercise08-A/src/program.py
@@ -6,18 +6,26 @@ import sys
 
 # Functions
 
-def count_steps(n: int, cache: dict[int, int]={}) -> int:
-    # TODO: Determine number of distinct ways to climb n steps using only
+
+def count_steps(n: int, cache: dict[int, int] = {0: 1, 1: 1}) -> int:
+    # Determine number of distinct ways to climb n steps using only
     # increments of 1 or 2 steps at a time.
-    return 0
+
+    if n not in cache:
+        cache[n] = count_steps(n - 1, cache) + count_steps(n - 2, cache)
+
+    return cache[n]
+
 
 # Main Execution
 
+
 def main() -> None:
-    for n in map(int, sys.stdin):
+    for n in (int(line) for line in sys.stdin):
         print(count_steps(n))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
