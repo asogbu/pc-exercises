d11ac26ff558b8fa141b35250ba5e35ecff7ad75
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-07-03 18:32:40 -0600

    auto format

--- exercise09-E/src/program.py
+++ exercise09-E/src/program.py
@@ -3,18 +3,21 @@
 # Exercise 09-E: BST Balanced
 
 from dataclasses import dataclass
-from typing      import Optional
+from typing import Optional
 import sys
 
 # Classes
 
+
 @dataclass
 class Node:
-    value:  int
-    left:   Optional['Node']
-    right:  Optional['Node']
+    value: int
+    left: Optional["Node"]
+    right: Optional["Node"]
+
 
 # Functions
+
 
 def bst_insert(node: Optional[Node], value: int) -> Optional[Node]:
     # Base: node doesn't exist, so return a new one
@@ -24,14 +27,15 @@ def bst_insert(node: Optional[Node], value: int) -> Optional[Node]:
     # Recursive: insert left if value is less than or equal to current Node,
     # otherwise insert right
     if value <= node.value:
-        node.left  = bst_insert(node.left, value)
+        node.left = bst_insert(node.left, value)
     else:
         node.right = bst_insert(node.right, value)
 
     return node
 
-def is_balanced(root: Optional[Node], height: int=0) -> tuple[bool, int]:
-    ''' Use divide and conquer to determine if a binary tree is balanced '''
+
+def is_balanced(root: Optional[Node], height: int = 0) -> tuple[bool, int]:
+    """Use divide and conquer to determine if a binary tree is balanced"""
     # Base Case: Invalid node, so return current height and that it is balanced
 
     # Divide and Conquer: Recursively compute height of left and right
@@ -40,7 +44,9 @@ def is_balanced(root: Optional[Node], height: int=0) -> tuple[bool, int]:
     # Combine: compute height and whether or not the overall tree is balanced
     return False, 0
 
+
 # Main Execution
+
 
 def main() -> None:
     for line in sys.stdin:
@@ -51,7 +57,8 @@ def main() -> None:
 
         # Determine if BST is balanced
         balanced, height = is_balanced(root)
-        print('Balanced' if balanced else 'Unbalanced')
+        print("Balanced" if balanced else "Unbalanced")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
