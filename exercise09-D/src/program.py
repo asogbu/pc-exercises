90d8494de9f84310022ad2b202f9280375b4bb9e
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-07-04 16:58:35 -0600

    Exercise 09-D: add Python implementation

--- exercise09-D/src/program.py
+++ exercise09-D/src/program.py
@@ -4,47 +4,70 @@
 
 from collections import deque
 from dataclasses import dataclass
-from typing      import Optional
+from typing import Optional, Iterator
 import sys
 
 # Classes
 
+
 @dataclass
 class Node:
-    value:  int
-    left:   Optional['Node']
-    right:  Optional['Node']
+    value: int
+    left: Optional["Node"] = None
+    right: Optional["Node"] = None
+
 
 # Functions
 
-def read_tree(values: list[int], index: int=0) -> Optional[Node]:
-    # TODO: Use divide and conquer to parse tree
-    return None
 
-def walk_tree(root: Optional[Node]) -> list[int]:
-    # TODO: Use BFS Traversal with Queue to create list of values
-    return []
+def read_tree(values: list[int], index: int = 0) -> Optional[Node]:
+    # Use divide and conquer to parse tree
+    if index >= len(values):
+        return None
+
+    root = Node(
+        values[index],
+        read_tree(values, 2 * index + 1),
+        read_tree(values, 2 * index + 2),
+    )
+
+    return root
+
+
+def walk_tree(root: Optional[Node]) -> Iterator[int]:
+    # Use BFS Traversal with Queue to create list of values
+    queue = deque([root])
+    while queue:
+        if node := queue.pop():
+            yield node.value
+            queue.appendleft(node.left)
+            queue.appendleft(node.right)
+
 
 def dump_tree(root: Optional[Node]) -> None:
-    print(','.join(map(str, walk_tree(root))))
+    print(",".join(str(n) for n in walk_tree(root)))
 
-def invert_tree(root: Optional[Node]) -> Optional[Node]:
-    # TODO: Use divide and conquer to invert binary tree
 
+def invert_tree(root: Optional[Node]) -> None:
+    # Use divide and conquer to invert binary tree
     # Base Case: Invalid Node
-
     # Divide and Conquer: Swap left and right and recurse
-    return None
+    if root:
+        invert_tree(root.left)
+        invert_tree(root.right)
+        root.left, root.right = root.right, root.left
+
 
 # Main Execution
 
+
 def main() -> None:
     for line in sys.stdin:
-        values = list(map(int, line.split()))
-        root   = read_tree(values)
+        values = [int(w) for w in line.split()]
+        tree = read_tree(values)
+        invert_tree(tree)
+        dump_tree(tree)
 
-        invert_tree(root)
-        dump_tree(root)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
