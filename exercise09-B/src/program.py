faf27874727803f05bd3cafad02b9689aef2dc7e
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-07-04 16:26:18 -0600

    Exercise 09-B: add Python implementation

--- exercise09-B/src/program.py
+++ exercise09-B/src/program.py
@@ -8,38 +8,53 @@ import sys
 
 # Structures
 
+
 @dataclass
 class Node:
     value: int
-    left:  Optional['Node']
-    right: Optional['Node']
+    left: Optional["Node"] = None
+    right: Optional["Node"] = None
+
 
 # Functions
 
+
 def minimum_tree(root: Optional[Node]) -> int:
-    ''' Use divide and conquer to compute the minimum of binary tree '''
+    """Use divide and conquer to compute the minimum of binary tree"""
     # Base case: Invalid Node
+    if not root:
+        return sys.maxsize
 
     # Divide and Conquer: Recursively solve left and right sub-trees
+    left_min = minimum_tree(root.left)
+    right_min = minimum_tree(root.right)
 
     # Combine: Take minimum of current node and left and right minimums
-    return 0
+    return min(root.value, left_min, right_min)
+
 
 # Main Execution
 
+
 def main() -> None:
     # Create tree
-    root = Node(7,
-        Node(5,
-                Node(3, None, None),
-                None),
-        Node(4,
-                None,
-                Node(2, None, None),
-    ))
+    root = Node(
+        7,
+        Node(
+            5,
+            Node(3),
+            None,
+        ),
+        Node(
+            4,
+            None,
+            Node(2),
+        ),
+    )
 
     # Compute minimum of tree
     print(minimum_tree(root))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
