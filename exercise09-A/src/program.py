c17abaad8d2b36f27bd57076d4c17c25793ecd93
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-07-04 16:16:26 -0600

    Exercise 09-A: add Python implementation

--- exercise09-A/src/program.py
+++ exercise09-A/src/program.py
@@ -7,50 +7,70 @@ from collections import deque
 
 # Node structure
 
+
 @dataclass
 class Node:
-    value:  str
-    left:   'Node' = None
-    right:  'Node' = None
+    value: str
+    left: "Node" = None
+    right: "Node" = None
+
 
 # Pre-defined Trees
 
-AlgorithmTree = \
-    Node('A',
-        Node('L',
-            Node('O',
-                Node('H'),
-                Node('M')
-            ),
-            Node('R'),
-        ),
-        Node('G',
-            Node('I'),
-            Node('T'),
-        ),
-    )
+AlgorithmTree = Node(
+    "A",
+    Node("L", Node("O", Node("H"), Node("M")), Node("R")),
+    Node("G", Node("I"), Node("T")),
+)
 
 # Traversal: BFS (Iterative)
 
+
 def bfs(root: Node) -> None:
-    pass
+    queue = deque([root])
+    while queue:
+        if node := queue.pop():
+            print(f" {node.value}", end="")
+            queue.appendleft(node.left)
+            queue.appendleft(node.right)
+
 
 # Traversal: DFS (Iterative)
 
+
 def dfs(root: Node) -> None:
-    pass
+    stack = [root]
+    while stack:
+        if node := stack.pop():
+            print(f" {node.value}", end="")
+            stack.append(node.right)
+            stack.append(node.left)
+
 
 # Traversal: DFS (Recursive)
 
+
 def dfs_recursive(root: Node) -> None:
-    pass
+    if root:
+        print(" " + root.value, end="")
+        dfs_recursive(root.left)
+        dfs_recursive(root.right)
+
 
 # Main Execution
 
-def main():
-    print(f'BFS:', end=''); bfs(AlgorithmTree); print()
-    print(f'DFS:', end=''); dfs(AlgorithmTree); print()
-    print(f'DFS:', end=''); dfs_recursive(AlgorithmTree); print()
 
-if __name__ == '__main__':
+def main():
+    print(f"BFS:", end="")
+    bfs(AlgorithmTree)
+    print()
+    print(f"DFS:", end="")
+    dfs(AlgorithmTree)
+    print()
+    print(f"DFS:", end="")
+    dfs_recursive(AlgorithmTree)
+    print()
+
+
+if __name__ == "__main__":
     main()
