f62e8435bc273542368309b4a0a16a972dea4b24
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-07-08 18:41:17 -0600

    Exercise 10-A: add Python implementation

--- exercise10-A/src/program.py
+++ exercise10-A/src/program.py
@@ -1,37 +1,79 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict, deque
-
 import sys
+from typing import Optional
 
 # Types
 
-Graph = dict[int, set[int]]
+Graph = dict[int, set]
 
 # Functions
 
 
-def read_graph() -> Graph:
+def read_graph(undirected: Optional[bool] = False) -> Graph:
     """Read graph from standard input"""
-    pass
+    graph: Graph = defaultdict(set)
+    for src, dst in (line.split() for line in sys.stdin):
+        graph[int(src)].add(int(dst))
+        if undirected:
+            graph[int(dst)].add(int(src))
+
+    return graph
 
 
-def walk_graph_dfs_rec(graph: Graph, v: int, visited: set[int]) -> None:
-    """Walk graph using DFS (recursive)"""
-    pass
+def walk_graph_dfs_rec(graph: Graph, v, visited: Optional[set] = None) -> None:
+    """Walk graph using DFS (recursive)
+
+    Args:
+        graph:      Adjacency list of graph we are traversing
+        v:          Vertex to visit
+        visited:    Vertices we have already processed
+    """
+
+    if visited is None:
+        visited = set()
+
+    # Base case: Already been visited
+    if v in visited:
+        return
+
+    # Recursive: process vertex, visit neighbors
+    visited.add(v)
+    print(v)
+    for neighbor in graph[v]:
+        walk_graph_dfs_rec(graph, neighbor, visited)
 
 
-def walk_graph_dfs_iter(graph: Graph, v: int) -> None:
+def walk_graph_dfs_iter(graph: Graph, v) -> None:
     """Walk graph using DFS (iterative)"""
-    pass
+    visited = set()
+    stack = [v]
+    while stack:
+        node = stack.pop()
+        if node in visited:
+            continue
+
+        visited.add(node)
+        print(node)
+        stack.extend(graph[node])
 
 
-def walk_graph_bfs_iter(graph: Graph, v: int) -> None:
+def walk_graph_bfs_iter(graph: Graph, v) -> None:
     """Walk graph using BFS (iterative)"""
-    pass
+    visited = set()
+    queue = deque([v])
+    while queue:
+        node = queue.pop()
+        if node in visited:
+            continue
+
+        visited.add(node)
+        print(node)
+        queue.extendleft(graph[node])
 
 
-def walk_graph(graph: Graph, root: int, walk: int):
+def walk_graph(graph: Graph, root, walk: int):
     if walk == 0:
         walk_graph_dfs_rec(graph, root, set())
     elif walk == 1:
