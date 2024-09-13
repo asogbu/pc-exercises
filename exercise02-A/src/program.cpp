51073924a691bca121ee570a5f524582779bb8c4
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-09-12 20:57:11 -0600

    Exercise 02-A: add C++ implementation

--- exercise02-A/src/program.cpp
+++ exercise02-A/src/program.cpp
@@ -1,17 +1,31 @@
 // Exercise 04-A: Anagrams
 
+#include <array>
+#include <cctype>
 #include <iostream>
-#include <vector>
+#include <string>
 
 // Constants
 
-const size_t NLETTERS = 26;
+constexpr size_t kNumLetters = 'Z' - 'A' + 1;
 
 // Functions
 
-bool is_anagram(std::string &s, std::string &t) {
-    // TODO: Return whether or not s and t are anagrams
-    return false;
+int GetLetterIndex(char c) {
+    return std::toupper(c) - 'A';
+}
+
+bool IsAnagram(const std::string &s, const std::string &t) {
+    if (s.length() != t.length())
+        return false;
+
+    std::array<int, kNumLetters> letter_counter_s = {0};
+    for (char c : s) letter_counter_s[GetLetterIndex(c)]++;
+
+    std::array<int, kNumLetters> letter_counter_t = {0};
+    for (char c : t) letter_counter_t[GetLetterIndex(c)]++;
+
+    return letter_counter_s == letter_counter_t;
 }
 
 // Main execution
@@ -20,7 +34,7 @@ int main(int argc, char *argv[]) {
     std::string first, second;
 
     while (std::cin >> first >> second) {
-        std::cout << (is_anagram(first, second) ? "Anagram" : "Not Anagram") << std::endl;
+        std::cout << (IsAnagram(first, second) ? "Anagram" : "Not Anagram") << '\n';
     }
 
     return 0;
