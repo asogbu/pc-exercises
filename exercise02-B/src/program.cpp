fe6d5bdf5c4965d8980234459156787f531295c4
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-09-13 15:58:22 -0600

    Exercise 02-B: add C++ implementation

--- exercise02-B/src/program.cpp
+++ exercise02-B/src/program.cpp
@@ -4,22 +4,44 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <unordered_map>
+
+// Constants
+
+const std::unordered_map<char, char> kMatching = {
+    {')', '('}, {']', '['}, {'}', '{'}};
 
 // Functions
 
-bool is_pbbmatched(const std::string &s) {
-    // TODO: Process string s using a stack to determine if the symbols are balanced
-    return false;
+bool IsPBBMatched(const std::string& s) {
+    std::stack<char> open_pbb;
+
+    for (char c : s) {
+        if (kMatching.find(c) != kMatching.end()) {
+            // c is a closing symbol
+            if (open_pbb.empty()) return false;
+            if (open_pbb.top() != kMatching.at(c)) return false;
+            open_pbb.pop();
+        } else {
+            // c is an opening symbol
+            open_pbb.push(c);
+        }
+    }
+
+    return open_pbb.empty();
 }
 
 // Main execution
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     std::string line;
 
     while (std::getline(std::cin, line)) {
-        std::cout << std::setw(10) << line << ": " << (is_pbbmatched(line) ? "Yes" : "No") << std::endl;
+        std::cout << std::setw(10) << line << ": "
+                  << (IsPBBMatched(line) ? "Yes" : "No") << '\n';
     }
 
     return 0;
 }
+
+// vim: set sts=4 sw=4 ts=8 ft=cpp:
