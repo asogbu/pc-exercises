65bd856d8f5b0a1ab6a42daa4f6380e67cc91919
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-09-13 22:40:56 -0600

    Exercise 04-A: add C++ implementation

--- exercise04-A/src/program.cpp
+++ exercise04-A/src/program.cpp
@@ -4,20 +4,37 @@
 #include <string>
 #include <unordered_set>
 
-// Functions
+// Prototypes
 
-bool is_palindromic(std::string &s) {
-    return false;
-}
+bool IsPalindromic(const std::string& s);
+void GetLettersWithOddCounts(const std::string& s, std::unordered_set<char>& odds);
+void FlipMembership(char c, std::unordered_set<char>& s);
 
 // Main execution
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     std::string word;
 
-    while (std::cin >> word) {
-        std::cout << (is_palindromic(word) ? "Yes" : "No") << std::endl;
-    }
+    while (std::cin >> word)
+        std::cout << (IsPalindromic(word) ? "Yes" : "No") << '\n';
 
     return 0;
 }
+
+// Functions
+
+bool IsPalindromic(const std::string& s) {
+    std::unordered_set<char> odds;
+    GetLettersWithOddCounts(s, odds);
+    return odds.size() <= 1;
+}
+
+void GetLettersWithOddCounts(const std::string& s, std::unordered_set<char>& odds) {
+    for (char c : s) FlipMembership(c, odds);
+}
+
+void FlipMembership(char c, std::unordered_set<char>& s) {
+    if (!s.insert(c).second) s.erase(c);
+}
+
+// vim: set sts=4 sw=4 ts=8 ft=cpp:
