3bb6a9380fba36195432882627f6f684137e3ca5
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-09-17 21:54:33 -0600

    Simplify input read and parsing

--- exercise01-A/src/program.cpp
+++ exercise01-A/src/program.cpp
@@ -1,17 +1,16 @@
 // Exercise 01-A: Line Totals
 
-#include <cctype>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 // Prototypes
 
-void LTrim(std::string &s);
-int GetStringSum(std::string s);
+int GetStringSum(const std::string& s);
 
 // Main Execution
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     std::string line;
 
     while (std::getline(std::cin, line))
@@ -22,23 +21,16 @@ int main(int argc, char *argv[]) {
 
 // Functions
 
-int GetStringSum(std::string s) {
+int GetStringSum(const std::string& s) {
     int sum = 0;
 
-    while (s.length() > 0) {
-        size_t pos;
-        sum += std::stoi(s, &pos);
-        s.erase(0, pos);
-        LTrim(s);
+    std::istringstream input(s);
+    int n;
+    while (input >> n) {
+        sum += n;
     }
 
     return sum;
-}
-
-void LTrim(std::string &s) {
-    size_t pos = 0;
-    while (pos < s.size() && std::isspace(s[pos])) pos++;
-    s.erase(0, pos);
 }
 
 // vim: set sts=4 sw=4 ts=8 ft=cpp:
