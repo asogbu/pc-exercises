8c27f15941591db50e8813514f25064136dac94e
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-09-17 23:17:16 -0600

    Exercise 04-C: add C++ implementation

--- exercise04-C/src/program.cpp
+++ exercise04-C/src/program.cpp
@@ -1,38 +1,65 @@
 // Exercise 04-C: Contiguous Array
 
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-// Functions
+// Prototypes
 
-std::vector<bool> read_digits(std::string &s) {
-    std::vector<bool> digits;
-    std::stringstream ss(s);
-    int digit;
+std::vector<bool> ReadDigits(const std::string &s);
 
-    while (ss >> digit) {
-        digits.push_back(digit);
-    }
-
-    return digits;
-}
-
-int find_maximum_contiguous_array(std::vector<bool> &digits) {
-    return 0;
-}
+// Finds the maximum length of a contiguous subarray with equal numbers of 0s and 1s
+size_t FindMaximumContiguousArray(const std::vector<bool> &digits);
 
 // Main execution
 
 int main(int argc, char *argv[]) {
     std::string line;
     while (getline(std::cin, line)) {
-        auto digits = read_digits(line);
-        auto length = find_maximum_contiguous_array(digits);
-        std::cout << length << std::endl;
+        std::vector<bool> digits = ReadDigits(line);
+        std::cout << FindMaximumContiguousArray(digits) << '\n';
     }
 
     return 0;
 }
+
+// Functions
+
+std::vector<bool> ReadDigits(const std::string &s) {
+    std::vector<bool> digits;
+    std::stringstream input(s);
+
+    int digit;
+    while (input >> digit) {
+        digits.push_back(digit);
+    }
+
+    return digits;
+}
+
+size_t FindMaximumContiguousArray(const std::vector<bool> &digits) {
+    size_t max_length = 0;
+
+    std::unordered_map<int, size_t> levels = {{0, 0}};
+    int curr_level = 0;
+    for (size_t i = 1; i <= digits.size(); i++) {
+        curr_level += (digits[i - 1] == 1) ? 1 : -1;
+
+        // Check if this balance has been seen before
+        if (levels.find(curr_level) != levels.end()) {
+            // Calculate the length of the subarray with equal 0s and 1s
+            size_t curr_length = i - levels[curr_level];
+            max_length = std::max(curr_length, max_length);
+        } else {
+            // Store the first occurrence of this balance
+            levels[curr_level] = i;
+        }
+    }
+
+    return max_length;
+}
+
+// vim: set sts=4 sw=4 ts=8 ft=cpp:
