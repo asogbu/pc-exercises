251ab44dded8241200a1e75df942b4f4a6e9af71
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2024-09-17 22:08:21 -0600

    Simplify input read, use C++ limits, minor changes

--- exercise01-D/src/program.cpp
+++ exercise01-D/src/program.cpp
@@ -1,27 +1,19 @@
 // Exercise 01-D: Count Duplicates
 
-#include <limits.h>
-
 #include <iostream>
 #include <istream>
-#include <string>
+#include <limits>
 
 // Prototypes
 
 int FindMatrixMaxRow(std::istream& input, int n);
-int GetStringSum(std::string s);
-void LTrim(std::string& s);
 
 // Main Execution
 
 int main(int argc, char* argv[]) {
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        int n = std::stoi(line);
-        if (n == 0) break;
-
-        std::cout << FindMatrixMaxRow(std::cin, n) << '\n';
-    }
+    int n;
+    while (std::cin >> n && n != 0)
+        std::cout << 1 + FindMatrixMaxRow(std::cin, n) << '\n';
 
     return 0;
 }
@@ -29,13 +21,16 @@ int main(int argc, char* argv[]) {
 // Functions
 
 int FindMatrixMaxRow(std::istream& input, int n) {
-    int max_sum = INT_MIN;
+    int max_sum = std::numeric_limits<int>::min();
     int max_row = 0;
 
-    std::string line;
-    for (int row = 1; row <= n; row++) {
-        std::getline(input, line);
-        int row_sum = GetStringSum(line);
+    for (int row = 0; row < n; row++) {
+        int row_sum = 0;
+        for (int col = 0; col < n; col++) {
+            int i;
+            input >> i;
+            row_sum += i;
+        }
 
         if (row_sum > max_sum) {
             max_sum = row_sum;
@@ -44,25 +39,6 @@ int FindMatrixMaxRow(std::istream& input, int n) {
     }
 
     return max_row;
-}
-
-int GetStringSum(std::string s) {
-    int sum = 0;
-
-    while (s.length() > 0) {
-        size_t pos;
-        sum += std::stoi(s, &pos);
-        s.erase(0, pos);
-        LTrim(s);
-    }
-
-    return sum;
-}
-
-void LTrim(std::string& s) {
-    size_t pos = 0;
-    while (pos < s.size() && std::isspace(s[pos])) pos++;
-    s.erase(0, pos);
 }
 
 // vim: set sts=4 sw=4 ts=8 ft=cpp:
